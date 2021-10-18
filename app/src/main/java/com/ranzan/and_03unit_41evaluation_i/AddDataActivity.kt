6461da1706ffaf8252e576b9231a7527de539c2a
RANZN <aryaprakashranjan@gmail.com>
2021-10-18 15:05:30 +0530

    addToDB

--- app/src/main/java/com/ranzan/and_03unit_41evaluation_i/AddDataActivity.kt
+++ app/src/main/java/com/ranzan/and_03unit_41evaluation_i/AddDataActivity.kt
@@ -1,11 +1,25 @@
 package com.ranzan.and_03unit_41evaluation_i
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ranzan.and_03unit_41evaluation_i.Database.DatabaseHelper
+import kotlinx.android.synthetic.main.activity_add_data.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class AddDataActivity : AppCompatActivity() {
+    private lateinit var dataBase: DatabaseHelper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_data)
+        dataBase = DatabaseHelper(this)
+        addDataBtn.setOnClickListener {
+            dataBase.addDBData(
+                eventName.text.toString(),
+                eventDate.text.toString(),
+                eventLocation.text.toString(),
+                eventDesc.text.toString(),
+                eventPrice.text.toString().toInt()
+            )
+        }
     }
 }
