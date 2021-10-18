6e66f1e979c304aa2a7d9ec627760ace334c4186
RANZN <aryaprakashranjan@gmail.com>
2021-10-18 14:43:11 +0530

    item_layout

--- app/src/main/java/com/ranzan/and_03unit_41evaluation_i/MainActivity.kt
+++ app/src/main/java/com/ranzan/and_03unit_41evaluation_i/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.ranzan.and_03unit_41evaluation_i
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        addDataBtn.setOnClickListener {
+            val intent = Intent(MainActivity@ this, AddDataActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/ranzan/and_03unit_41evaluation_i/RecyclerViewAdapter.kt
@@ -0,0 +1,4 @@
+package com.ranzan.and_03unit_41evaluation_i
+
+class RecyclerViewAdapter {
+}
